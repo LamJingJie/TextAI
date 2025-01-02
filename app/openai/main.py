f2792a99b7f4a98a9e687b848c7a7cf37fa32190
jingjie <jingjie105@gmail.com>
2025-01-03 01:46:34 +0800

    feat: img desc & keywords using openai API.

--- app/openai/main.py
+++ app/openai/main.py
@@ -1,6 +1,8 @@
 from openai import OpenAI, AsyncOpenAI, OpenAIError
 from dotenv import load_dotenv, dotenv_values 
 import os
+import base64
+import json
 
 # Initialize openai client
 # Use gpt-4o vision model
@@ -12,8 +14,83 @@ import os
 # Considerations
 # - Using batch processing (Takes up to 24hrs but much cheaper)
 
+
 async def initialize_openAI():
     client = AsyncOpenAI(
         api_key = os.getenv('OPENAI_API_KEY')
     )
     return client
+
+
+async def get_openai_response(client: AsyncOpenAI, new_img: str,  student_name: str, curr_page: str, prj_title: str, submission_date: str, desc: str, img_id: str, output: dict):
+    content = {
+        "desc": "",
+        "keywords": []
+    }
+
+    completion = await client.chat.completions.create(
+        model="gpt-4o-2024-11-20",
+        messages = [
+            {
+                "role": "developer",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": """Interpret the hidden meanings, emotions, and messages conveyed by the abstract image. Provide a detailed explanation, including any possible symbolic or metaphorical interpretations. 
+                                    Provide a list of keywords and a description. If the image contains text, return the text as the description and extract keywords from it. 
+                                    Minimum 1 keyword, maximum 5 keywords. Minimum 40 description words.""",
+                    },
+                    {
+                        "type": "context",
+                        "context": f"Consider the following context: Student: {student_name}, Project title: {prj_title}, Page: {curr_page}, Context of the image: {desc}",
+                    }
+                ],
+
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{new_img}",
+                            "detail": "high", # 768 x 2048, expensive but btr quality output
+                        }
+                    }
+                ]
+            },
+        ],
+
+        response_format = {
+            "type": "json_schema",
+            "json_schema": {
+                "name": "image_content",
+                "strict": True, # Enforce the response schema strictly
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "desc": {
+                            "description": "Description of the abstract image",
+                            "type": "string"
+                        },
+
+                        "keywords": {
+                            "description": "Keywords of the abstract image",
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            },
+                        }
+                    },
+                    "required": ["desc", "keywords"],
+                    "additionalProperties": False # Disallow properties not defined in the schema
+                }
+            }
+        },
+
+        max_tokens=3000,
+    )
+    # Convert json str to dict
+    response_data: dict = json.loads(completion.choices[0].message.content)
+    content["desc"] = response_data["desc"]
+    content["keywords"] = response_data["keywords"]
+    key = f"{prj_title}__{curr_page}__{submission_date}__{student_name}__{img_id}"
+    output[key] = content
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv, dotenv_values 
 import asyncio
-from app.openai.main import initialize_openAI
+from app.openai.main import initialize_openAI, AsyncOpenAI, get_openai_response
 from app.playwright.main import get_page_data_playwright, get_all_pages_playwright
 from multiprocessing import Lock, Pool, Manager, cpu_count, Queue
 from concurrent.futures import ProcessPoolExecutor, Future, wait
@@ -17,7 +17,7 @@ from io import BytesIO
 # 3. Initialize openai client (Done)
 # 4. retrieve json data from tldraw using playwright (Done)
 # 5. Get relevent data from json (Done)
-# 5. Send data to openai (URL or Base64) (Resize them before sending)
+# 5. Send data to openai (URL or Base64) (Resize them before sending) (DONE)
 # 6. Get response from openai
 # 7. Process response (figure out how to store the desc and keywords for each of the img)
 
@@ -29,7 +29,7 @@ from io import BytesIO
 
 async def main(processors: ProcessPoolExecutor):
     pages_json_content: list = []
-    client = await initialize_openAI()
+    client: AsyncOpenAI = await initialize_openAI()
     targets, url = await cmd_user_input()
 
     # Get relevent JSON data from tldraw
@@ -42,19 +42,39 @@ async def main(processors: ProcessPoolExecutor):
     pages_json_content = await asyncio.gather(*pages_json_content)
 
     futures: list[Future] = []
-    processed_data = {}
 
-    # Processing images
+    # Process imgs in each page
     for page in pages_json_content:
-
         for img in page['all_student_imgs']:
-            # Process all imgs seperately, in parallel
-            future = processors.submit(process_img_openai, img[0], page['assets'], img[1], page['target'], page['date'], page['desc'], processed_data)
+            # Transform all imgs seperately, in PARALLEL (multi-processing)
+            future = processors.submit(process_img, img[0], page['assets'], img[1], page['target'], page['prj_title'], page['date'], page['desc'])
             futures.append(future)
     
+
     # Wait for all images to be processed
     wait(futures, return_when="ALL_COMPLETED")
-    print("All images processed")
+
+    openai_img_output: dict = {}
+    tasks: list = []
+
+    # Send imgs to openai asynchronously
+    for future in futures:
+        new_img, student_name, curr_page, prj_title, submission_date, desc, student_img_id = future.result()
+
+        if new_img:
+            # Send img to openAI 4o vision model
+            tasks.append(get_openai_response(client, new_img, student_name, curr_page, prj_title, submission_date, desc, student_img_id, openai_img_output))
+
+
+    # Wait for all imgs to be processed by openAI, returns back an array[obj]
+    await asyncio.gather(*tasks)
+    print(openai_img_output)
+    
+    # save output to a json file
+    
+
+
+    
     
     
 
@@ -67,46 +87,48 @@ async def main(processors: ProcessPoolExecutor):
 # d) Store response and that img name, along with student name. Store as JSON file
 
 # Process 1 image at a time
-def process_img_openai(student_img_id, assets, student_name, page, submission_date, prj_desc, processed_data):
+def process_img(student_img_id: str, assets, student_name, curr_page, prj_title, submission_date, desc) -> tuple:
     for asset in assets:
         if student_img_id == asset['id']:
-            new_img: base64 = resize_img(asset['props']['src'])
-
-            # Send img to openAI vision model
+            new_img: str = resize_img(asset['props']['src'])
             
+            # student_img_id = student_img_id[student_img_id.find(":") + 1:]
+            student_img_id = student_img_id[len("asset:"):]
+            return new_img, student_name, curr_page, prj_title, submission_date, desc, student_img_id
+
 
 
 # Resize img to fit openAI vision model specs
-def resize_img(img_data: str | URL) -> base64:
+def resize_img(img_data: str | URL) -> str:
 
     # Check if url or base64 and get the raw bytes data
     if img_data.startswith('data:image') and img_data.find('base64,') != -1:
         # Is base64
         base64_index = img_data.find('base64,') + len('base64,')
-        img_data = base64.b64decode(img_data[base64_index:]) # raw byte data
+        img_data = base64.b64decode(img_data[base64_index:]) # raw bytes data
     else:
         # is URL
         response: Response = requests.get(img_data)
         if response.status_code == 200:
-            img_data = response.content # raw byte data
+            img_data = response.content # raw bytes data
         else:
             # Unable to retrieve img data (bytes) from url
             print("Fail")
 
             # <Error handling here>
-            return
+            return None
     
     # Convert raw byte into file-like object to be opened by PIL as an image
     image = Image.open(BytesIO(img_data))
-    max_res_horizontal: tuple = (768, 2000)
-    max_res_vertical: tuple = (2000, 768)
+    max_res_horizontal: tuple = (768, 2048)
+    max_res_vertical: tuple = (2048, 768)
     buffered = BytesIO()
 
-    # Resize img for 'high res' mode. Short side <= 768px and Long side <= 2000px
+    # Resize img for 'high res' mode. Short side <= 768px and Long side <= 2048
     image.thumbnail(max_res_horizontal, Image.LANCZOS) if image.size[0] < image.size[1] else image.thumbnail(max_res_vertical, Image.LANCZOS)
-    image.save(buffered, format="JPEG", quality = 95, subsampling = 0) # 0 = highest quality slowest, 2 = lowest quality but fastest
+    image.save(buffered, format="PNG", quality = 95, subsampling = 0) # 0 = highest quality slowest, 2 = lowest quality but fastest
     img_bytes: bytes = buffered.getvalue()
-    return base64.b64encode(img_bytes)
+    return base64.b64encode(img_bytes).decode('utf-8')
         
     
 
